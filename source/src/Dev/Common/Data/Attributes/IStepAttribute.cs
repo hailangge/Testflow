d7d771fa89e26f9a6e411de8c0ebd2487d215e90
jingtao <tao.jing@jytek.com>
2020-09-14 17:34:27 +0800

    新增IStepAttribute和IStepAttributeCollection的定义

---
+++ source/src/Dev/Common/Data/Attributes/IStepAttribute.cs
@@ -0,0 +1,40 @@
+﻿using Testflow.Data.Sequence;
+
+namespace Testflow.Data.Attributes
+{
+    /// <summary>
+    /// Step属性
+    /// </summary>
+    public interface IStepAttribute : ISequenceElement
+    {
+        /// <summary>
+        /// 属性名称
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// 属性的目标名称
+        /// </summary>
+        string Target { get; set; }
+
+        /// <summary>
+        /// 属性的类型名称
+        /// </summary>
+        string Type { get; set; }
+
+        /// <summary>
+        /// 完整类型
+        /// </summary>
+        string FullType { get; set; }
+
+        /// <summary>
+        /// 属性的触发条件
+        /// </summary>
+        string Condition { get; set; }
+
+        /// <summary>
+        /// 属性的值，可以包含变量、表达式等
+        /// </summary>
+        string Value { get; set; }
+    }
+}
---
+++ source/src/Dev/Common/Data/Attributes/IStepAttributeCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Testflow.Data.Attributes
+{
+    /// <summary>
+    /// 步骤属性集合
+    /// </summary>
+    public interface IStepAttributeCollection : IList<IStepAttribute>
+    {
+         
+    }
+}
