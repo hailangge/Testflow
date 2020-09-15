b1b9370dc7965081abe34f121ba8afa00c5595b6
jingtao <tao.jing@jytek.com>
2020-09-15 15:46:57 +0800

    新增ActionResult类用于从Action获取执行结果信息

---
+++ source/src/Dev/UserLibInterface/Actions/ActionResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Testflow.Runtime;
+
+namespace Testflow.ExtensionBase.Actions
+{
+    /// <summary>
+    /// Action执行结果
+    /// </summary>
+    public class ActionResult
+    {
+        /// <summary>
+        /// Action执行是否通过
+        /// </summary>
+        public bool IsPassed { get; set; }
+
+        /// <summary>
+        /// 失败类型
+        /// </summary>
+        public FailedType FailedType { get; set; }
+
+        /// <summary>
+        /// 错误异常
+        /// </summary>
+        public Exception Exception { get; set; }
+
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        public string ErrorMessage { get; set; }
+
+        /// <summary>
+        /// 错误码
+        /// </summary>
+        public int ErrorCode { get; set; }
+    }
+}
