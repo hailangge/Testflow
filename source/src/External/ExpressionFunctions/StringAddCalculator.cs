d35ead38bcfc6cb66a901d5339a27bc4f8915550
jingtao <tao.jing@jytek.com>
2021-03-26 18:02:24 +0800

    新增字符串的加法计算器

---
+++ source/src/External/ExpressionFunctions/StringAddCalculator.cs
@@ -0,0 +1,24 @@
+﻿using Testflow.Data.Expression;
+
+namespace Testflow.External.ExpressionCalculators
+{
+    public class StringAddCalculator : IExpressionCalculator
+    {
+        public StringAddCalculator()
+        {
+            Operator = "Addition";
+        }
+
+        public string Operator { get; }
+
+        public bool IsCalculable(object sourceValue, params object[] arguments)
+        {
+            return sourceValue is string && arguments[0] is string;
+        }
+
+        public object Calculate(object sourceValue, params object[] arguments)
+        {
+            return sourceValue.ToString() + arguments[0];
+        }
+    }
+}
