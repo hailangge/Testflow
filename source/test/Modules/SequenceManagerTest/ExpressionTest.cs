9b4362db5365ec6d26f6e160ded201ae07112dda
jingtao <tao.jing@jytek.com>
2021-04-02 18:11:16 +0800

    提交SequenceManager模块表达式解析的单元测试，暂时未实现

---
+++ source/test/Modules/SequenceManagerTest/ExpressionTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testflow.SequenceManagerTest
+{
+    [TestClass]
+    public class ExpressionTest
+    {
+        [TestMethod]
+        public void ParseSimpleExpression()
+        {
+            
+        }
+    }
+}
