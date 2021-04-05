1a6de303c78660473bff5b35a4b521b18fc41085
massena <jingtaobupt@hotmail.com>
2021-04-05 11:09:05 +0800

    提交简单的表达式单元测试(仅用于简单测试，未添加到CUnit单元)

---
+++ source/test/TestflowLoaderTest/SystemTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Testflow.Data.Expression;
+using Testflow.Loader;
+using Testflow.Utility.Expression;
+
+namespace Testflow.TestflowLoaderTest
+{
+    public class SystemTest
+    {
+        public static void Main(string[] args)
+        {
+            Regex regex = new Regex("[abc]");
+            string[] strings = regex.Split("1a2b3c");
+
+            TestflowRunnerOptions runnerOptions = new TestflowRunnerOptions();
+            TestflowRunner runnerInstance = TestflowActivator.CreateRunner(runnerOptions);
+            runnerInstance.Initialize();
+            runnerInstance.DesigntimeInitialize();
+            Dictionary<string, ExpressionOperatorInfo> operatorInfos =
+                    runnerInstance.SequenceManager.ConfigData.GetProperty<Dictionary<string, ExpressionOperatorInfo>>("ExpressionOperators");
+            ExpressionParser parser = new ExpressionParser(operatorInfos);
+            IExpressionData expressionData = parser.ParseExpression("a[1, 2] -(- b) [1E-10]");
+            IExpressionData expressionData1 = parser.ParseExpression("e ? a[1, 2] -(- b) [1E-10] : c");
+            IExpressionData expressionData2 = parser.ParseExpression("e ? a[1, 2] -(- b) [1E-10] : c*d");
+            IExpressionData expressionData5 = parser.ParseExpression("(e ? a[1, 2] -(- b) [1E-10] : c)*d");
+            IExpressionData expressionData3 = parser.ParseExpression("a[1, 1-a*b+2] -(- b) [1E-10*5]");
+            IExpressionData expressionData4 = parser.ParseExpression("a[1, 1-a*b+2] -(- b) [1E-10*5]+");
+        }
+    }
+}
