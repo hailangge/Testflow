96ac092e4ca60994cc396a033df75c9195074eb7
massena <jingtaobupt@hotmail.com>
2021-04-18 17:35:29 +0800

    表达式新增支持数值到字符串的格式化计算

---
+++ source/src/External/ExpressionFunctions/NumericToStringFormatter.cs
@@ -0,0 +1,27 @@
+﻿using Testflow.Data.Expression;
+
+namespace Testflow.External.ExpressionCalculators
+{
+    public class NumericToStringFormatter : IExpressionCalculator
+    {
+        public NumericToStringFormatter()
+        {
+            Operator = "StringFormat";
+        }
+
+        public string Operator { get; }
+
+        public bool IsCalculable(object sourceValue, params object[] arguments)
+        {
+            return sourceValue is double && arguments[0] is string;
+        }
+
+        public object Calculate(object sourceValue, params object[] arguments)
+        {
+            string format = arguments[0] as string;
+            return string.IsNullOrWhiteSpace(format)
+                ? ((double) sourceValue).ToString()
+                : ((double) sourceValue).ToString(format) + arguments[0];
+        }
+    }
+}
